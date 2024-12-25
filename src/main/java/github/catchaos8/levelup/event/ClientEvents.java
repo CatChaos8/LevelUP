69743fc6491ddc5762cd7bb9575e67e773f25e81
CatChaos_8 <chaospatato@gmail.com>
2024-12-25 08:30:48 -0500

    0.1.0 First Working ish version!

--- src/main/java/github/catchaos8/levelup/event/ClientEvents.java
+++ src/main/java/github/catchaos8/levelup/event/ClientEvents.java
@@ -12,23 +12,24 @@ import net.minecraftforge.fml.common.Mod;
 
 public class ClientEvents {
 
-
+    @Mod.EventBusSubscriber(modid = levelUP.MOD_ID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
 
         @SubscribeEvent
         public static void onKeyInput(InputEvent.Key event){
+
             if(KeyBinding.OPEN_KEY.consumeClick()) {
                 Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed open GUI key"));
             }
         }
-        
+
     }
 
     @Mod.EventBusSubscriber(modid = levelUP.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModBusEvents {
 
         @SubscribeEvent
-        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
             event.register(KeyBinding.OPEN_KEY);
         }
 
--- src/main/java/github/catchaos8/levelup/levelUP.java
+++ src/main/java/github/catchaos8/levelup/levelUP.java
@@ -3,12 +3,11 @@ package github.catchaos8.levelup;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 @Mod(levelUP.MOD_ID)
 public class levelUP {
     public static final String MOD_ID = "levelup";
-
+    public static final Logger LOGGER = LogUtils.getLogger();
     //test
 }
