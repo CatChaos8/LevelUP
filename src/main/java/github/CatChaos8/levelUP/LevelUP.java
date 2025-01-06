6f0d1cbb76e9643a939fb4ff2d3a353ec1e01186
CatChaos8 <chaospatato@gmail.com>
2025-01-05 19:51:49 -0500

    v0.2.2 Leveling system finished Now I just need to make gui and make the stats actually do stuff

--- src/main/java/github/CatChaos8/levelUP/LevelUP.java
+++ src/main/java/github/CatChaos8/levelUP/LevelUP.java
@@ -1,11 +1,15 @@
 package github.catchaos8.levelup;
 
 import com.mojang.logging.LogUtils;
+import github.catchaos8.levelup.config.LevelUPClientConfig;
+import github.catchaos8.levelup.config.LevelUPCommonConfig;
 import github.catchaos8.levelup.networking.ModNetwork;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -22,6 +26,9 @@ public class LevelUP {
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, LevelUPClientConfig.SPEC, "levelup-client.toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, LevelUPCommonConfig.SPEC, "levelup-common.toml");
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
